993bb4efc9b1da13d8f12f057e595beef7c37c02
christiano39 <carneson39@gmail.com>
2020-09-30 11:53:41 -0600

    delete irrelevent comment

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -15,13 +15,3 @@ if __name__ == "__main__":
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
-
-# cache = {}
-
-# def fib(n):
-#     if n <= 1: return n
-
-#     if n not in cache:
-#         cache[n] = fib(n-1) + fib(n-2)
-    
-#     return cache[n]
