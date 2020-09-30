c112f7b0c8928e560523fae1e1d2c010a6d7dff9
christiano39 <carneson39@gmail.com>
2020-09-30 10:46:02 -0600

    implement expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,13 @@
 # Your code here
-
+cache = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    if x <= 0: return y + z
 
+    if (x, y, z) not in cache:
+        cache[(x, y, z)] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
 
+    return cache[(x, y, z)]
 
 if __name__ == "__main__":
     for i in range(10):
@@ -12,3 +15,13 @@ if __name__ == "__main__":
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
+
+# cache = {}
+
+# def fib(n):
+#     if n <= 1: return n
+
+#     if n not in cache:
+#         cache[n] = fib(n-1) + fib(n-2)
+    
+#     return cache[n]
