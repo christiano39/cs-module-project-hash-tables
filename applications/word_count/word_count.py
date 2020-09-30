3c07ed936d6854dc70082069323207a6c486547c
christiano39 <carneson39@gmail.com>
2020-09-30 14:47:49 -0600

    implement word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,26 @@
+import re
+
 def word_count(s):
-    # Your code here
+    delimeters = " ", "\n", "\t", "\r"
+    regex_pattern = '|'.join(map(re.escape, delimeters))
+
+    words = re.split(regex_pattern, s)
+    count = {}
+    for word in words:
+        alpha_word = ''
+        for c in word:
+            if c not in '":;,.-+=/\\|[]{}()*^&':
+                alpha_word += c
+        if alpha_word == '':
+            continue
+        alpha_word = alpha_word.lower()
+        if alpha_word in count:
+            count[alpha_word] += 1
+        else:
+            count[alpha_word] = 1
+    return count
+
+
 
 
 
@@ -8,3 +29,5 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
